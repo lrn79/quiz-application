71cefd418dd6618a14199c5baa612bb9d40de94a
Mohammad Shariful Islam <shariful@xpeedstudio.com>
2023-08-10 23:27:03 +0600

    project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+const App = () => {
+  return (
+    <div>
+      <h3 className="text-3xl">My Quiz App</h3>
+      <Button type='primary' className='m-4'>Next</Button>
+    </div>
+  );
+};
+
+export default App;
