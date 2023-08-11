2d9b904bb66d41f44040982a2963dfdbf91f8c4b
Mohammad Shariful Islam <shariful@xpeedstudio.com>
2023-08-11 22:57:54 +0600

    not found page set

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,8 +1,15 @@
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
+
     return (
-        <div>
-            <h2>Not Found Page</h2>
+        <div className='w-full h-screen flex justify-center items-center'>
+            <div>
+                <Button type='primary'>
+                    <Link replace to='/'>Back To Home</Link>
+                </Button>
+            </div>
         </div>
     );
 };
